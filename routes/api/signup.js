50cd7842a4657da6cc7d89960699d3efff2a5545
Costopoulos <dkns99@gmail.com>
2021-06-23 23:27:51 +0300

    Fuck heroku 2

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../db.js");
+const pool = require("../../db");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 // var redis = require('redis');
