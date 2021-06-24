32952f853b830bae58702a1ba03f08941711cf1b
Costopoulos <dkns99@gmail.com>
2021-06-24 16:37:15 +0300

    lmao

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const sessionPool = require('pg').Pool;
     password: "2a3abd7d2c0566e916516bb702af3c6fd6076f6249b756f7639323712a8cfde",
     host: "ec2-3-212-75-25.compute-1.amazonaws.com",
     port: 5432,
-    database: "d8vre040lu8r33"
+    database: "public"
   });
 
   
