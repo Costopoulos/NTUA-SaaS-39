31c7b7a63a78dcfb199d9e01007edaed50880d24
Costopoulos <dkns99@gmail.com>
2021-06-24 04:09:02 +0300

    Try sth insane

--- index.js
+++ index.js
@@ -14,12 +14,19 @@ const sessionPool = require('pg').Pool;
 
 
 
+  // const sessionDBaccess = new sessionPool({
+  //   user: "postgres",
+  //   password: "68709900",
+  //   host: "localhost",
+  //   port: 5432,
+  //   database: "soa_db"
+  // });
   const sessionDBaccess = new sessionPool({
-    user: "postgres",
-    password: "68709900",
-    host: "localhost" || process.env.DATABASE_URL || "ec2-3-212-75-25.compute-1.amazonaws.com",
+    user: "urpisvtgytavek",
+    password: "2a3abd7d2c0566e916516bb702af3c6fd6076f6249b756f7639323712a8cfde",
+    host: "ec2-3-212-75-25.compute-1.amazonaws.com",
     port: 5432,
-    database: "soa_db"
+    database: "d8vre040lu8r33"
   });
 
   
