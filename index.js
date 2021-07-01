58b472b470a22e80fe5b95a108892c06e856a80e
Costopoulos <dkns99@gmail.com>
2021-07-01 20:23:12 +0300

    MVC -> not secure ofc

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ const sessionPool = require('pg').Pool;
       cookie: {
           maxAge: 1000 * 60 * 60 * 24 * 7,
           aameSite: true, //aameSite
-          // secure: false // ENABLE ONLY ON HTTPS
-          secure: true
+          secure: false // ENABLE ONLY ON HTTPS
       }};
 
 // const fs = require('fs');
