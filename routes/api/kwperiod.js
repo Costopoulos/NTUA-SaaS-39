3813c011e58cb8f8016611628c02fe9fb3168825
Costopoulos <dkns99@gmail.com>
2021-07-06 18:54:50 +0300

    Wrong include

--- routes/api/kwperiod.js
+++ routes/api/kwperiod.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../db");
+const pool = require("../../originaldb");
 const authorization = require("../../middleware/authorization");
 require('dotenv').config();
 
