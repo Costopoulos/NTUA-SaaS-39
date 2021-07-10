ce8da4139228d638292410eeb16ce5d6bb5cad5c
Costopoulos <dkns99@gmail.com>
2021-07-10 19:27:31 +0300

    Add signup implementation

---
+++ MicroServices/auth-service/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+require('dotenv').config();
+
+
+//middleware
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Signup
+app.use("/signup", require("./routes/api/signup.js"));
+
+//Signin
+// app.use("/signin", require("./routes/api/signin.js"));
+
+//Signout
+// app.use("/signout", require("./routes/api/signout.js"));
+
+//Port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server has started at port ${port}`);
+});
--- MicroServices/auth-service/routes/api/signup.js
+++ MicroServices/auth-service/routes/api/signup.js
@@ -1,91 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../db");
+const pool = require("../../database.js");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
-
-// router.post("/", async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const newUser = await pool.query(
-//       "INSERT INTO mvcuser (email, password) VALUES ($1, $2) RETURNING *",
-//       [email, password]
-//     );
-//     res.json(newUser.rows[0]);
-//   } catch (err) {
-//     res.status(400).json({ Message: "User already exists" });
-//   }
-// });
-
-// bcrypt.genSalt(10).then(result => { //genSalt generates a hash of 10 digits
-//     let password1 = bcrypt.hash(password, result)
-// }).then(password1 => {
-//     const newUser = pool.query(
-//         "INSERT INTO mvcuser (email, password) VALUES ($1, $2) RETURNING *",
-//         [email, password1]
-//     )
-
-// }).then(res.json(newUser.rows[0]))
-
-// const salt = bcrypt.genSalt();
-// let password1 = bcrypt.hash(password, salt);
-
-//   const newUser = await pool.query(
-//     `INSERT INTO mvcuser (email, password) VALUES (${email}, crypt:(${password}, gen_salt('bf', 8))`
-//   );
-//   res.json(newUser.rows[0]);
-
-//THIS
-
-// router.post("/", async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const salt = await bcrypt.genSalt();
-//     console.log(salt);
-//     let password1 = await bcrypt.hash(password, salt);
-//     //console.log(password1);
-//     const newUser = await pool.query(
-//       "INSERT INTO mvcuser (email, password) VALUES ($1, $2) RETURNING *",
-//       [email, password1]
-//     );
-//     // console.log()
-//     res.json(newUser.rows[0]);
-//   } catch (err) {
-//     res.status(400).json({ Message: "User already exists" });
-//   }
-// });
 
 router.post("/", async (req, res) => {
   try {
     const { email, password1, password2 } = req.body;
+    if (email.length === 0 || password1.length === 0){
+      return res.status(400).json({Message: "Fill all fields!"})
+    }
+
     if (password1 !== password2){
-      req.flash("successMessage", "Λανθασμένος συνδυασμός κωδικών")
-      return res.redirect("/signup")
+      return res.status(400).json({Message: "Passwords do not match"})
     }
 
     const salt = await bcrypt.genSalt();
     const sela = await Promise.all([bcrypt.hash(password1, salt)]);
     const newUser = await pool.query(
-      "INSERT INTO soauser (email, password) VALUES ($1, $2) RETURNING *;",
+      "INSERT INTO auth_user (email, password) VALUES ($1, $2) RETURNING *;",
       [email, sela[0]]
     );
 
-    // console.log(newUser.rows[0].id);
-
-    // res.json(newUser.rows[0]);
-    //create token
-    // const payload = {
-    //   user: newUser.rows[0].user_id
-    // }
-    // token = jwt.sign(payload, process.env.jwtSecret, {expiresIn: 300});
-    // // console.log(token)
-    // res.json({token});
-
-    req.flash("successMessage", "Successful Signup")
-    return res.redirect("/signin");
-
+    res.json({Message: "Successfully signed up"})
 
   } catch (err) {
     res.status(400).json({ Message: "User with this email already exists" });
