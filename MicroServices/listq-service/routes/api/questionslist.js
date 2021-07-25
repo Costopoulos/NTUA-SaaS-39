2db3139334be1c086be484d98c720988e1a4a737
Costopoulos <dkns99@gmail.com>
2021-07-26 00:20:45 +0300

    Updated questionslist

--- MicroServices/listq-service/routes/api/questionslist.js
+++ MicroServices/listq-service/routes/api/questionslist.js
@@ -11,7 +11,7 @@ router.get("/", async (req,res) => {
 
         console.log(allquestions);
 
-        console.log(json(allquestions));
+        console.log(res.json(allquestions));
         
         return res.json(allquestions)
 
