a4270783ec0068da2ec02107ab92c4461d7c1302
Costopoulos <dkns99@gmail.com>
2021-07-25 22:22:51 +0300

    Update database for listquestions service

--- MicroServices/listq-service/database.sql
+++ MicroServices/listq-service/database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE listq_mss;
 CREATE TABLE questions(
     question_id int generated always as identity,
     user_id int,
-    user_email citext,
+    user_email varchar(255),
     title varchar(255) not null,
     question_text text not null,
     keywords text[],
@@ -14,7 +14,7 @@ CREATE TABLE questions(
 CREATE TABLE answers(
     answer_id serial primary key,
     user_id int,
-    user_email citext,
+    user_email varchar(255),
     question_id int,
     answer_text text not null,
     answered_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
