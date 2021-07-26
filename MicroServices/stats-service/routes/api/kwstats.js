347a38e530944c6ec1fe2df5efe4c4f834e6f174
Costopoulos <dkns99@gmail.com>
2021-07-26 18:10:12 +0300

    Finish stats service

--- MicroServices/stats-service/routes/api/kwstats.js
+++ MicroServices/stats-service/routes/api/kwstats.js
@@ -1,4 +1,3 @@
-  
 const express = require("express");
 const router = express.Router();
 const pool = require("../../database");
@@ -8,9 +7,11 @@ router.get("/", async (req,res) => {
     
     const graphkeywords = await pool.query(
         "select keyword, counter from (select keyword, count(*) as counter from (select unnest(keywords) as keyword from questions) as g group by keyword) as k order by counter desc limit 5;"
-    )
+    );
 
-    return res.json({graphkeywords});
+    // console.log(graphkeywords.rows);
+
+    return res.json(graphkeywords.rows);
 });
 
 module.exports = router;
