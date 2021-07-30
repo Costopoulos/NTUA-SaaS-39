2c1d00f1c0abda87e291f69a42cd46063626fcd6
Costopoulos <dkns99@gmail.com>
2021-07-30 13:50:07 +0300

    Almost finishing quesitonsanswer

--- routes/api/questionsanswer.js
+++ routes/api/questionsanswer.js
@@ -1,4 +1,5 @@
 const { default: axios } = require("axios");
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const pool = require("../../database");
@@ -14,37 +15,60 @@ router.get("/:id", async (req,res) => {
     const question_id = req.params.id;
 
     try {
-        const question = axios.get(`http://localhost:7000/getonequestion/${question_id}`)
+        let data = []
+        let data1 = []
+        axios.get(`http://localhost:7000/getonequestion/${question_id}`)
         .then((response)=>{
-            console.log("mia erwthsh "+JSON.stringify(response.data));
+            // console.log("mia erwthsh "+JSON.stringify(response.data));
             // return res.json({allquestions: response.data})
-            const thequestion = response.data
+            // const thequestion = response.data
+            data.push(response.data)
+        })
+        .then((response) => {
+            axios.get(`http://localhost:7000/answerquestion/${question_id}`)
+            .then((resp) => {
+                // console.log(resp);
+                // return resp.data
+                data.push(resp.data)
+                // console.log(data[0]);
+            })
+            .then((responsara) => {
+                // console.log(data);
+                // console.log(data1[0]);
+                // data.push(responsara)
+                // console.log(responsara);
+                console.log(data);
+                res.render("questionsanswer.ejs", {qid: question_id, question: data[0], answers: Object.keys(data[0]), successMessage: req.flash("successMessage"), errorMessage: req.flash("errorMessage")})
+            })
+        })
+        
     
-        }, (error) => {
-            // console.log(error.response.data['Message']);
-            console.log(error);
-            // return res.status(400).send(error.response.data);
-        });
+        // }, (error) => {
+        //     // console.log(error.response.data['Message']);
+        //     console.log(error);
+        //     // return res.status(400).send(error.response.data);
+        // });
 
         // // console.log(question);
 
-        const answers = axios.get(`http://localhost:7000/answerquestion/${question_id}`)
-        .then((response)=>{
-            console.log("apanthseis "+response.data);
-            // return res.json({allquestions: response.data})
-    
-        }, (error) => {
-            // console.log(error.response.data['Message']);
-            return res.status(400).send(error.response.data);
-        });
+        // axios.get(`http://localhost:7000/answerquestion/${question_id}`)
+        // .then((response)=>{
+        //     // console.log("apanthseis "+JSON.stringify(response.data));
+        //     // return res.json({allquestions: response.data})
 
-        axios.all([question,answers]).then(axios.spread((...responses) => {
-            const response1 = responses[0]
-            const response2 = responses[1]
-            return res.render("questionsanswer.ejs", {qid: question_id, question: question, answers: answers.rows, successMessage: req.flash("successMessage"), errorMessage: req.flash("errorMessage")})
-        })).catch(error => {
-            console.log(error);
-        })
+    
+        // }, (error) => {
+        //     // console.log(error.response.data['Message']);
+        //     return res.status(400).send(error.response.data);
+        // });
+
+        // axios.all([question,answers]).then(axios.spread((...responses) => {
+        //     const response1 = responses[0]
+        //     const response2 = responses[1]
+        //     return res.render("questionsanswer.ejs", {qid: question_id, question: question, answers: answers.rows, successMessage: req.flash("successMessage"), errorMessage: req.flash("errorMessage")})
+        // })).catch(error => {
+        //     console.log(error);
+        // })
 
         // // return res.render("questionsanswer.ejs", {qid: question_id, question: question.rows[0], answers: answers.rows, successMessage: req.flash("successMessage"), errorMessage: req.flash("errorMessage")})
     
@@ -80,7 +104,7 @@ router.post("/:id", async (req, res) => {
 
         const user_id = req.session.user.id;
 
-        axios.post(`http://localhost:7000/questionsanswer/:${question_id}`,{
+        axios.post(`http://localhost:7000/answerquestion/${question_id}`,{
             user_id: user_id,
             user_email: user_email,
             answertext: answertext
