3de979ba70037f6fc673348fc633e5c4528e20a0
Costopoulos <dkns99@gmail.com>
2021-07-31 11:14:21 +0300

    Deploy auth service

--- MicroServices/auth-service/database.js
+++ MicroServices/auth-service/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "yscrsqrqrvuzcb",
-  password: "5cac20d5345a990c0560563dd9de2af2df83461893588f1c4d5144079e533c88",
-  host: "ec2-35-174-122-153.compute-1.amazonaws.com",
+  user: "xityqvrswzktsz",
+  password: "d179b0fd8761d837d94919a954914f130df4a1294d7ea17796a66f446b4dcca2",
+  host: "ec2-54-220-170-192.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "dbmes7s4tuvsng",
+  database: "d1sl608ocrsoha",
   ssl: { rejectUnauthorized: false}
 });
 
