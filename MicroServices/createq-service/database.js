132cbed66b884fd95a8f69d751e672981e92d0cc
Costopoulos <dkns99@gmail.com>
2021-07-31 11:18:27 +0300

    Deploy create questions service

--- MicroServices/createq-service/database.js
+++ MicroServices/createq-service/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "ahvawpiqbwmgfv",
-  password: "acf6cc460f3fed6a49bbb0ea08341dbd9a3e621341a19d325aa08492c09df29b",
-  host: "ec2-34-228-100-83.compute-1.amazonaws.com",
+  user: "ltbvlpjezckgvd",
+  password: "2ef04af8ee7290df9e9146d75dd7a5eb3b1a01b656b590d67766f5b6bd1268ad",
+  host: "ec2-54-216-48-43.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "d49egcsotl6tln",
+  database: "dd7lt56fj09kos",
   ssl: { rejectUnauthorized: false}
 });
 
