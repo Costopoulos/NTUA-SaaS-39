60748933d08343e86f79ce8cf3d94624bb1c9984
Costopoulos <dkns99@gmail.com>
2021-07-31 11:44:20 +0300

    Deploy dashboard service

--- MicroServices/dashboard-service/database.js
+++ MicroServices/dashboard-service/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "rdonaodfhfvfsr",
-  password: "743a9a5edf54aa7171e95eb8e4737856eda2f8a7b60ae2b12bd990a119514855",
-  host: "ec2-54-236-137-173.compute-1.amazonaws.com",
+  user: "jowiotiyyvgmlu",
+  password: "e317e879d0d90563eded194c1b629488dcbc09fcd471f8c151a31debc26aed67",
+  host: "ec2-54-220-53-223.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "d756tuh07o357c",
+  database: "dedlhegsq1d85s",
   ssl: { rejectUnauthorized: false}
 });
 
