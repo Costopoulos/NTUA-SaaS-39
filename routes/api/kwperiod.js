ad505abc4ded9be4b378421aeb00a1a2d6c25970
Costopoulos <dkns99@gmail.com>
2021-07-31 16:14:19 +0300

    Add footer everywhere, add about&contact pages, fix time bug of stats per period in client

--- routes/api/kwperiod.js
+++ routes/api/kwperiod.js
@@ -11,7 +11,7 @@ router.get("/", async (req,res) => {
         axios.get("https://saas39-event-bus.herokuapp.com/statisticsperperiod")
         .then((response) => {
             console.log(response.data);
-            return res.render("kwperiod.ejs", {day1: response.data.day1, day2: response.data.day2,day3: response.data.day3,day4: response.data.day4,day5: response.data.day5,day6: response.data.day6,day7: response.data.day7, successMessage: req.flash("successMessage"), errorMessage: req.flash("errorMessage")})
+            return res.render("kwperiod.ejs", {day1: response.data.day1, day2: response.data.day2,day3: response.data.day3,day4: response.data.day4,day5: response.data.day5,day6: response.data.day6,day7: response.data.day7, lday1: response.data.lday1,lday2: response.data.lday2,lday3: response.data.lday3,lday4: response.data.lday4,lday5: response.data.lday5,lday6: response.data.lday6, lday7:response.data.lday7, successMessage: req.flash("successMessage"), errorMessage: req.flash("errorMessage")})
         }, (error) => {
             console.log(error);
             return res.status(400).send(error);
