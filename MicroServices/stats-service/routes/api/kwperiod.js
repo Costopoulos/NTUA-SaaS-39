e55e6632d15bdebe2f889401b768952a54a91231
Costopoulos <dkns99@gmail.com>
2021-07-31 16:09:03 +0300

    Fix time bug in kwperio

--- MicroServices/stats-service/routes/api/kwperiod.js
+++ MicroServices/stats-service/routes/api/kwperiod.js
@@ -29,8 +29,32 @@ router.get("/", async (req,res) => {
         const day1 = await pool.query(
             "SELECT count(*) FROM questions WHERE asken_on >= NOW() - interval '1 week' and asken_on <= NOW() - interval '6 days';"
         )
-    
-        return res.json({day1: day1.rows, day2: day2.rows,day3: day3.rows,day4: day4.rows,day5: day5.rows,day6: day6.rows,day7: day7.rows});
+
+        
+        const lday7 = await pool.query(
+            "SELECT EXTRACT(month from NOW()) AS month, EXTRACT(day FROM NOW()) AS day;"
+        )
+        const lday6 = await pool.query(
+            "SELECT EXTRACT(month from NOW() - interval '1 day') AS month, EXTRACT(day FROM NOW() - interval '1 day') AS day;"
+        )
+        const lday5 = await pool.query(
+            "SELECT EXTRACT(month from NOW() - interval '2 days') AS month, EXTRACT(day FROM NOW() - interval '2 days') AS day;"
+        )
+        const lday4 = await pool.query(
+            "SELECT EXTRACT(month from NOW() - interval '3 days') AS month, EXTRACT(day FROM NOW() - interval '3 days') AS day;"
+        )
+        const lday3 = await pool.query(
+            "SELECT EXTRACT(month from NOW() - interval '4 days') AS month, EXTRACT(day FROM NOW() - interval '4 days') AS day;"
+        )
+        const lday2 = await pool.query(
+            "SELECT EXTRACT(month from NOW() - interval '5 days') AS month, EXTRACT(day FROM NOW() - interval '5 days') AS day;"
+        )
+        const lday1 = await pool.query(
+            "SELECT EXTRACT(month from NOW() - interval '6 days') AS month, EXTRACT(day FROM NOW() - interval '6 days') AS day;"
+        )
+
+
+        return res.json({day1: day1.rows, day2: day2.rows,day3: day3.rows,day4: day4.rows,day5: day5.rows,day6: day6.rows,day7: day7.rows, lday1: lday1.rows,lday2: lday2.rows,lday3: lday3.rows,lday4: lday4.rows,lday5: lday5.rows,lday6: lday6.rows, lday7:lday7.rows});
 
     } catch (error) {
         console.error(err.message);
