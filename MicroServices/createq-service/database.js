e948fbaff76d3a48615184f830f443208d8a536c
Costopoulos <costopoulos.constantinos@gmail.com>
2023-02-06 22:51:39 +0200

    Change db creds in the createq service, due to heroku deleting the db

--- MicroServices/createq-service/database.js
+++ MicroServices/createq-service/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "ltbvlpjezckgvd",
-  password: "2ef04af8ee7290df9e9146d75dd7a5eb3b1a01b656b590d67766f5b6bd1268ad",
-  host: "ec2-54-216-48-43.eu-west-1.compute.amazonaws.com",
+  user: "jsjgbyiwgteaas",
+  password: "ce23a0e33256ff48ada51f16faaaa5b3ba4f0e9f32627eb41a8ac232155a6c50",
+  host: "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "dd7lt56fj09kos",
+  database: "d300somcme3kg0",
   ssl: { rejectUnauthorized: false}
 });
 
