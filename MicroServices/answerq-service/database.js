53c67d1daf3abe3c551dca94b949f652ce1506f5
Costopoulos <costopoulos.constantinos@gmail.com>
2024-01-14 16:20:34 +0200

    Attach new db to answerq-service

--- MicroServices/answerq-service/database.js
+++ MicroServices/answerq-service/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "wycpqsrjevbhzc",
-  password: "f1458bf6f5762c847b11062bb37c4a040e20bbdccbdf667fe215f0b05f9d9679",
-  host: "ec2-52-30-67-143.eu-west-1.compute.amazonaws.com",
+  user: "tsljtfeuhktiyq",
+  password: "43bc79f7d6c5862b8fa8291efda5e6e8c336a13ec81e603d62af15031b392507",
+  host: "ec2-54-155-46-64.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "d8ucha9co2hpsa",
+  database: "d6p0pafqlpv73l",
   ssl: { rejectUnauthorized: false}
 });
 
