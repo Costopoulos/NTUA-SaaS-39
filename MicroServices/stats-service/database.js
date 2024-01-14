a8e0399ee9e5423e9cc2e9ef8ce792386cf98f16
Costopoulos <costopoulos.constantinos@gmail.com>
2024-01-14 16:17:33 +0200

    Attach new db to stats-service

--- MicroServices/stats-service/database.js
+++ MicroServices/stats-service/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "hcudrhjhqxgdvl",
-  password: "bda97797bd8b5a8f4ec412b567824f7c38706a8633b91098b8a325aca78bc7cf",
-  host: "ec2-52-30-67-143.eu-west-1.compute.amazonaws.com",
+  user: "mjpmwtpybcecvd",
+  password: "842487045a2f4080bc5f94f10077ad54ef3b82bbb7d395b0f8c17c621deac863",
+  host: "ec2-54-155-46-64.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "d4sv7itns0ic0v",
+  database: "d40615lc0gucoe",
   ssl: { rejectUnauthorized: false}
 });
 
