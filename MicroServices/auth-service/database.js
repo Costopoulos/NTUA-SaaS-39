8f990bf0119f05f0b73ccec52f53c90c6a0e200b
Costopoulos <costopoulos.constantinos@gmail.com>
2024-01-14 15:30:12 +0200

    Attach new db to auth-service

--- MicroServices/auth-service/database.js
+++ MicroServices/auth-service/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "dcyfemwoeqbjko",
-  password: "92e6990f69a796240972cc1063b1d51a22bf33304445864a85d28484d654efa1",
-  host: "ec2-52-30-67-143.eu-west-1.compute.amazonaws.com",
+  user: "lyixxejbcsarkm",
+  password: "8f739f33087389a37e37d11116ddb12cf4492aa041f7be9f0bbd1318c2b2e1ad",
+  host: "ec2-34-242-199-141.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "d5qjkgkd12c240",
+  database: "dddfe30qe27juh",
   ssl: { rejectUnauthorized: false}
 });
 
