8278a986cf408fbea25a77fe545b6960c575a54a
Costopoulos <costopoulos.constantinos@gmail.com>
2024-01-14 17:19:59 +0200

    Optimize database schemas for client-service & listq-service

--- MicroServices/listq-service/database.sql
+++ MicroServices/listq-service/database.sql
@@ -10,13 +10,3 @@ CREATE TABLE questions(
     asken_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     primary key(question_id)
 );
-
-CREATE TABLE answers(
-    answer_id serial primary key,
-    user_id int,
-    user_email varchar(255),
-    question_id int,
-    answer_text text not null,
-    answered_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    constraint fk_question foreign key(question_id) references questions(question_id) on delete no action
-);
